c18a9523a31d93545dbb2a2f8a19956fbf282201
Ondřej Langr <hi@ondrejlangr.cz>
2024-11-12 08:27:29 +0100

    Updates prettier configs and updates workspace

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,15 @@
-import { config } from '@apitree.cz/prettier-config';
+import { config } from "@apitree.cz/prettier-config";
 
 export default {
   ...config,
-  printWidth: 90,
-  importOrder: ['<THIRD_PARTY_MODULES>', '^../', '^[./]', '^@/'],
+  printWidth: 50,
+  importOrder: ["<THIRD_PARTY_MODULES>", "^../", "^[./]", "^@/"],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
   importOrderCaseInsensitive: true,
-  plugins: ['@trivago/prettier-plugin-sort-imports', ...config.plugins],
+  plugins: [
+    "@trivago/prettier-plugin-sort-imports",
+    "prettier-plugin-packagejson",
+    ...config.plugins,
+  ],
 };

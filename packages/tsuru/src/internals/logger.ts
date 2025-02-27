042c6a0f1cf7e57c9a0e26fe578736bfbce99396
Ondřej Langr <hi@ondrejlangr.cz>
2025-02-27 22:43:11 +0100

    Fixes pipelines

--- packages/tsuru/src/internals/logger.ts
+++ packages/tsuru/src/internals/logger.ts
@@ -2,41 +2,65 @@ import colors from 'picocolors';
 
 import type { JsonLikeObject } from '../schemas/jsonLikeObjectSchema.js';
 
-const runWhenDebugEnabled =
-  <T extends (...params: any[]) => any>(action: T) =>
-  (...params: Parameters<T>) => {
-    if (!process.argv.includes('--debug')) {
+const isDebugEnabled = process.argv.includes('--debug');
+
+type LoggerHandler = (
+  message: string,
+  meta?: JsonLikeObject,
+) => void;
+
+enum MessageTypes {
+  DEBUG = 'debug',
+  ERROR = 'error',
+  INFO = 'info',
+  WARN = 'warn',
+  SUCCESS = 'success',
+}
+
+type TypesToColor = Record<
+  MessageTypes,
+  (message: string) => string
+>;
+const typeToColor: TypesToColor = {
+  [MessageTypes.DEBUG]: colors.blue,
+  [MessageTypes.ERROR]: colors.red,
+  [MessageTypes.INFO]: colors.blue,
+  [MessageTypes.WARN]: colors.yellow,
+  [MessageTypes.SUCCESS]: colors.green,
+};
+
+const createMessage = (
+  type: MessageTypes,
+  message: string,
+  meta?: JsonLikeObject,
+) => {
+  const withColor = typeToColor[type];
+  const prefix = `@tsuru ${type}:`;
+
+  return `${withColor(prefix)} ${message}${meta ? ` ${JSON.stringify(meta, null, 2)}` : ''}`;
+};
+
+const createLog =
+  (type: MessageTypes): LoggerHandler =>
+  (message, meta) => {
+    if (type === MessageTypes.DEBUG && !isDebugEnabled) {
       return;
     }
 
-    action(...params);
+    console.log(createMessage(type, message, meta));
   };
 
-export const logger = {
-  debug: runWhenDebugEnabled(
-    (message: string, meta?: JsonLikeObject) =>
-      console.log(
-        `${colors.blue('@tsuru debug:')} ${message}${meta ? ` ${JSON.stringify(meta, null, 2)}` : ''}`,
-      ),
-  ),
-
-  error: (message: string, meta?: JsonLikeObject) =>
-    console.log(
-      `${colors.red('@tsuru error:')} ${message}${meta ? ` ${JSON.stringify(meta, null, 2)}` : ''}`,
-    ),
-
-  info: (message: string, meta?: JsonLikeObject) =>
-    console.log(
-      `${colors.blue('@tsuru info:')} ${message}${meta ? ` ${JSON.stringify(meta, null, 2)}` : ''}`,
-    ),
-
-  warn: (message: string, meta?: JsonLikeObject) =>
-    console.log(
-      `${colors.yellow('@tsuru warn:')} ${message}${meta ? ` ${JSON.stringify(meta, null, 2)}` : ''}`,
-    ),
-
-  success: (message: string, meta?: JsonLikeObject) =>
-    console.log(
-      `${colors.green('@tsuru success:')} ${message}${meta ? ` ${JSON.stringify(meta, null, 2)}` : ''}`,
-    ),
+interface Logger {
+  debug: LoggerHandler;
+  error: LoggerHandler;
+  info: LoggerHandler;
+  warn: LoggerHandler;
+  success: LoggerHandler;
+}
+export const logger: Logger = {
+  debug: createLog(MessageTypes.DEBUG),
+  error: createLog(MessageTypes.ERROR),
+  info: createLog(MessageTypes.INFO),
+  warn: createLog(MessageTypes.WARN),
+  success: createLog(MessageTypes.SUCCESS),
 };
